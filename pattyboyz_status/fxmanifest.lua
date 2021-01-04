c4389afbdb6d87a74267d8d20d7a472a0a5f49ea
pattyboyz_github <62997197+Pattyboyz@users.noreply.github.com>
2021-01-04 14:31:12 +0700

    Add files via upload

---
+++ pattyboyz_status/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+version '1.0.0'
+
+ui_page "html/index.html"
+
+files {
+    "html/index.html",
+    "html/main.css",
+    "html/status.js"
+}
+
+client_script {
+    'client/*.lua',
+    'config.lua',
+}
